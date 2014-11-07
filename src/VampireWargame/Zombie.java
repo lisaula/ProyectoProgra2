1666ff15047e50c3a680421c2a08e01f956953d7
Luis Carlos Isaula <luisca_elfather@hotmail.com>
2014-11-07 16:12:21 -0600

    funcion atacar zombie y crear zombie

---
+++ src/VampireWargame/Zombie.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package VampireWargame;
+
+/**
+ *
+ * @author admin
+ */
+public class Zombie extends Ficha {
+    
+    public Zombie(String c){
+        super(1,1,0,c);
+    }
+    
+    
+}
